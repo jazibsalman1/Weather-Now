700d57a731c82ddee40feeeae169eedcab1f1dce
jazibsalman1 <jazib1341@gmail.com>
2025-08-24 14:44:31 +0500

    port error solved for render

--- app.py
+++ app.py
@@ -47,4 +47,4 @@ async def get_weather(request: Request, city: str = Form(...)):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", --port $PORT)
